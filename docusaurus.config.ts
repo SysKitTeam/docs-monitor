9472988cc096cdb441277625e8b40f8e681ea560
imesaric <igor.mesaric.w@outlook.com>
2025-09-26 16:12:15 +0200

    title fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'SPDockit Consultant',
+  title: 'Syskit Monitor',
   tagline: 'SharePoint management and administration tool',
   favicon: 'img/favicon.ico',
 
